da88b18d63004bf3599439fdafafb000de12e531
Liudongya <1570964489@qq.com>
2018-07-27 18:22:05 +0800

    Initial commit

---
+++ src/com/hiveview/crm/test/test.java
@@ -0,0 +1,8 @@
+package com.hiveview.crm.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World 0000000");
+        System.out.println("Hello");
+    }
+}
